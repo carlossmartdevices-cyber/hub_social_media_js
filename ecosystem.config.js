bb22289705bd1d5e973af5ea84e94aca85ccd333
Carlos Smartdevices <carlos@smartdevices.com>
2025-12-17 18:50:27 +0000

    bbbb

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,18 +17,6 @@ module.exports = {
       }
     },
     {
-      name: "twitter-auth",
-      script: "dist/platforms/twitter/TwitterAdapter.js",
-      disable_trace: true,
-      env: {
-        NODE_ENV: process.env.NODE_ENV || "production",
-        DB_HOST: "localhost",
-        DB_PORT: "55432",
-        REDIS_HOST: "localhost",
-        REDIS_PORT: "6379"
-      }
-    },
-    {
       name: "clickera-client",
       script: "npm",
       args: "start",
