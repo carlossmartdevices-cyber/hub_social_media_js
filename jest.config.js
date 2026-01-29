6e6516e3b08eba9f466dff947c095d71812bcdb2
carlossmartdevices-cyber <carlossmartdevices@gmail.com>
2026-01-29 04:43:43 -0500

    Exclude integration tests from default Jest run

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/tests/integration',
+    '<rootDir>/tests/load',
+    '<rootDir>/tests/e2e',
+  ],
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
   },
